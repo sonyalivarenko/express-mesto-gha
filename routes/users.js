5d58070052974f75dd4f312fca995aeb63636dc4
Sofia Lyvarenko <livarenko.sofia@gmail.com>
2023-03-16 14:31:46 +0300

    добавление авторизации

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/order */
 const routerUser = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUserId, updateAvatar, updateProfile, getСurrentUser,
 } = require('../controllers/users');
@@ -7,7 +8,16 @@ const {
 routerUser.get('/', getUsers);
 routerUser.get('/:userId', getUserId);
 routerUser.get('/me', getСurrentUser);
-routerUser.patch('/me', updateProfile);
-routerUser.patch('/me/avatar', updateAvatar);
+routerUser.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
+routerUser.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(/https?:\/\/w*[-._~:/?#[\]@!$&'()*+,;=0-9a-z]+#?/i),
+  }),
+}), updateAvatar);
 
 module.exports = { routerUser };

6da8793ac43460b4dfc5e246b63d0aa8cfb6bb97
Sofia Lyvarenko <livarenko.sofia@gmail.com>
2023-03-16 16:18:00 +0300

    редактирование кода

--- controllers/cards.js
+++ controllers/cards.js
@@ -16,8 +16,8 @@ module.exports.getCards = (req, res, next) => {
 module.exports.deleteCard = (req, res, next) => {
   const { cardId } = req.params;
   const { userId } = req.user._id;
+  Card.findById(cardId).populate('owner');
   Card.findByIdAndRemove(cardId)
-    .populate('owner')
     .then((card) => {
       if (card) {
         const { owner } = card.owner._id;

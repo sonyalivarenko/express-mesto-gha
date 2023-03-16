7e8d7fdf1559913170c45bded359067320d0b176
Sofia Lyvarenko <livarenko.sofia@gmail.com>
2023-03-16 15:01:15 +0300

    добавление авторизации

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const DocumentNotFoundError = require('../errors/DocumentNotFoundError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -41,12 +41,12 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new DocumentNotFoundError('Неправильные почта или пароль'));
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new DocumentNotFoundError('Неправильные почта или пароль'));
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
           }
           return user;
         });
--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,18 @@
 /* eslint-disable linebreak-style */
 const routerCard = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, deleteCard, createCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 routerCard.get('/', getCards);
 routerCard.delete('/:cardId', deleteCard);
-routerCard.post('/', createCard);
+routerCard.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+}), createCard);
 routerCard.put('/:cardId/likes', likeCard);
 routerCard.delete('/:cardId/likes', dislikeCard);
 
